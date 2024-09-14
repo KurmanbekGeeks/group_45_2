09f57430930793f31561485f792d95fbfc87126f
Kurmanbek-1 <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>
2024-09-14 13:59:00 +0600

    4 lesson

---
+++ main.py
@@ -0,0 +1,25 @@
+import logging
+from aiogram.utils import executor
+from buttons import start_test
+from config import bot, dp, admin
+from handlers import commands, echo, quiz, FSM_registrtion, fsm_store
+from db import db_main
+
+async def on_startup(_):
+    for i in admin:
+        await bot.send_message(chat_id=i, text="Бот включен!",
+                               reply_markup=start_test)
+        await db_main.sql_create()
+
+
+commands.register_commands(dp)
+quiz.register_quiz(dp)
+FSM_registrtion.register_fsm_reg(dp)
+fsm_store.register_store(dp)
+
+echo.register_echo(dp)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
