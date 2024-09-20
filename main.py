911132a8d75b9ba0e50146ffa6fc43851b80b677
Kurmanbek-1 <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>
2024-09-20 21:08:17 +0600

    lesson-6

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import logging
 from aiogram.utils import executor
 from buttons import start_test
 from config import bot, dp, admin
-from handlers import commands, echo, quiz, FSM_registrtion, fsm_store
+from handlers import commands, echo, quiz, FSM_registrtion, fsm_store, webapp, admin_group
 from db import db_main
+
 
 async def on_startup(_):
     for i in admin:
@@ -16,9 +17,11 @@ commands.register_commands(dp)
 quiz.register_quiz(dp)
 FSM_registrtion.register_fsm_reg(dp)
 fsm_store.register_store(dp)
+webapp.register_handlers_webapp(dp)
 
-echo.register_echo(dp)
 
+admin_group.register_admin_group(dp)
+# echo.register_echo(dp)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
